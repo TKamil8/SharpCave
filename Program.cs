df475433a288ba9dc3d4b14acc785fd3c6d26faa
TKamil8 <kamil.tarko@wp.pl>
2023-10-09 18:33:34 +0200

    Add RPS gameplay

--- Program.cs
+++ Program.cs
@@ -1,4 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Let's play Rock-Paper-Scissors!");
+﻿Console.WriteLine("Let's play Rock-Paper-Scissors!");
+
+Console.WriteLine("Provide sign, first player:");
+string? firstPlayerSign = Console.ReadLine();
+
+Console.WriteLine("Provide sign, second player:");
+string? secondPlayerSign = Console.ReadLine();
+
+if (firstPlayerSign == secondPlayerSign)
+{
+    Console.WriteLine("It's a draw!");
+}
+else if (firstPlayerSign == "rock" && secondPlayerSign == "scissors")
+{
+    Console.WriteLine("First player won!");
+}
+else if(firstPlayerSign == "paper" && secondPlayerSign == "rock")
+{
+    Console.WriteLine("First player won!");
+}
+else if(firstPlayerSign == "scissors" && secondPlayerSign == "paper")
+{
+    Console.WriteLine("First player won!");
+}
+else
+{
+    Console.WriteLine("Second player won!");
+}
+
 Console.WriteLine("Press Enter to close the game...");
 Console.ReadLine();
