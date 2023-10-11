12009a6766194df0f9431afae3f00a3e5bb58c7c
TKamil8 <kamil.tarko@wp.pl>
2023-10-11 17:46:44 +0200

    Loop gameplay until any player writes 'quit'

--- Program.cs
+++ Program.cs
@@ -1,31 +1,43 @@
 ﻿Console.WriteLine("Let's play Rock-Paper-Scissors!");
 
-Console.WriteLine("Provide sign, first player:");
-string? firstPlayerSign = Console.ReadLine();
+while (true)
+{
+    Console.WriteLine("Provide sign, first player (or write 'quit' to end game):");
+    string? firstPlayerSign = Console.ReadLine();
 
-Console.WriteLine("Provide sign, second player:");
-string? secondPlayerSign = Console.ReadLine();
+    if (firstPlayerSign == "quit")
+    {
+        break;
+    }
 
-if (firstPlayerSign == secondPlayerSign)
-{
-    Console.WriteLine("It's a draw!");
-}
-else if (firstPlayerSign == "rock" && secondPlayerSign == "scissors")
-{
-    Console.WriteLine("First player won!");
-}
-else if(firstPlayerSign == "paper" && secondPlayerSign == "rock")
-{
-    Console.WriteLine("First player won!");
-}
-else if(firstPlayerSign == "scissors" && secondPlayerSign == "paper")
-{
-    Console.WriteLine("First player won!");
-}
-else
-{
-    Console.WriteLine("Second player won!");
-}
+    Console.WriteLine("Provide sign, second player (or write 'quit' to end game):");
+    string? secondPlayerSign = Console.ReadLine();
 
+    if (secondPlayerSign =="quit")
+    {
+        break;
+    }
+
+    if (firstPlayerSign == secondPlayerSign)
+    {
+        Console.WriteLine("It's a draw!");
+    }
+    else if (firstPlayerSign == "rock" && secondPlayerSign == "scissors")
+    {
+        Console.WriteLine("First player won!");
+    }
+    else if (firstPlayerSign == "paper" && secondPlayerSign == "rock")
+    {
+        Console.WriteLine("First player won!");
+    }
+    else if (firstPlayerSign == "scissors" && secondPlayerSign == "paper")
+    {
+        Console.WriteLine("First player won!");
+    }
+    else
+    {
+        Console.WriteLine("Second player won!");
+    }
+}
 Console.WriteLine("Press Enter to close the game...");
 Console.ReadLine();
