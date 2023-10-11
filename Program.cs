4118375e9514ad3e383714d10698fd406cfe5d50
TKamil8 <kamil.tarko@wp.pl>
2023-10-11 18:38:45 +0200

    Check sign correctness

--- Program.cs
+++ Program.cs
@@ -2,16 +2,26 @@
 
 while (true)
 {
-    Console.WriteLine("Provide sign, first player (or write 'quit' to end game):");
-    string? firstPlayerSign = Console.ReadLine();
+    string? firstPlayerSign;
+    do
+    {
+        Console.WriteLine("Provide sign, first player (or write 'quit' to end game):");
+        firstPlayerSign = Console.ReadLine();
+
+
+    } while (firstPlayerSign != "rock" && firstPlayerSign != "paper" && firstPlayerSign != "scissors" && firstPlayerSign != "quit");
 
     if (firstPlayerSign == "quit")
     {
         break;
     }
 
-    Console.WriteLine("Provide sign, second player (or write 'quit' to end game):");
-    string? secondPlayerSign = Console.ReadLine();
+    string? secondPlayerSign;
+    do
+    {
+        Console.WriteLine("Provide sign, second player (or write 'quit' to end game):");
+        secondPlayerSign = Console.ReadLine();
+    } while (secondPlayerSign != "rock" && secondPlayerSign != "paper" && secondPlayerSign != "scissors" && secondPlayerSign != "quit");
 
     if (secondPlayerSign == "quit")
     {
