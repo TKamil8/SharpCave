8bd6538d7751304a72741a972f35e02fc1f53088
TKamil8 <kamil.tarko@wp.pl>
2023-10-15 10:22:03 +0200

    Trim whitespaces on provided signs

--- Program.cs
+++ Program.cs
@@ -18,7 +18,7 @@ Console.WriteLine($" Round {roundNumber}");
         do
         {
             Console.WriteLine($"Provide sign, first player (or write '{EndGameCommand}' to end game):");
-            firstPlayerSign = Console.ReadLine()?.ToLower();
+            firstPlayerSign = Console.ReadLine()?.ToLower().Trim();
         } while (!availableSigns.Contains(firstPlayerSign) && firstPlayerSign != EndGameCommand);
 
         if (firstPlayerSign == EndGameCommand)
@@ -31,7 +31,7 @@ Console.WriteLine($" Round {roundNumber}");
         do
         {
             Console.WriteLine($"Provide sign, second player (or write '{EndGameCommand}' to end game):");
-            secondPlayerSign = Console.ReadLine()?.ToLower();
+            secondPlayerSign = Console.ReadLine()?.ToLower().Trim();
         } while (!availableSigns.Contains(secondPlayerSign) && secondPlayerSign != EndGameCommand);
 
         if (secondPlayerSign == EndGameCommand)
